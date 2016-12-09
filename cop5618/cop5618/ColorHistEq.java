adee0747e4c56a76d38c8eb53f91ee2bff9d0583
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2016-12-09 18:44:57 -0500

    Commented code cleared.

--- cop5618/cop5618/ColorHistEq.java
+++ cop5618/cop5618/ColorHistEq.java
@@ -76,31 +76,6 @@ public class ColorHistEq {
 
 		double[] binsArray = binsMap.entrySet().stream().map(Map.Entry::getValue).mapToDouble(hsbCounts -> hsbCounts)
 				.toArray();
-		/*
-		 * System.out.println("\n\nThe binsCount are: \n\n" + binsMap.get(0) +
-		 * ", "+binsMap.get(1)+ ", "+ binsMap.get(2) + ", "+ binsMap.get(3) +
-		 * ", "+ binsMap.get(4) + "\n\n");
-		 */
-		/*
-		 * collect(Collectors.groupingBy(hsb -> { if (hsb.b <= 0.1) { return 0;
-		 * } else if (hsb.b <= 0.2) { return 1; } else if (hsb.b <= 0.3) {
-		 * return 2; } else if (hsb.b <= 0.4) { return 3; } else if (hsb.b <=
-		 * 0.5) { return 4; } else if (hsb.b <= 0.6) { return 5; } else if
-		 * (hsb.b <= 0.7) { return 6; } else if (hsb.b <= 0.8) { return 7; }
-		 * else if (hsb.b <= 0.9) { return 8; } else { return 9; } }));
-		 */
-
-		// time.now(); // convert to HSB and create brightness map
-
-		/*
-		 * // count of pixels in each histogram bin double[] binsArray =
-		 * binsMap.entrySet().stream().map(Map.Entry::getValue).mapToDouble(
-		 * hsbList -> hsbList.size()) .toArray();
-		 * 
-		 * System.out.println("\n\nThe binsCount are: \n\n" + binsArray[0] +
-		 * ", "+binsArray[1]+ binsArray[2] + ", "+ binsArray[3] + ", "+
-		 * binsArray[4] + "\n\n");
-		 */
 
 		// calculate prefix sum
 		Arrays.parallelPrefix(binsArray, (x, y) -> x + y);
@@ -122,28 +97,6 @@ public class ColorHistEq {
 			return histInstance.new HSBPixel(hsbFloatArrayofPixel);
 		}).mapToInt(hsb -> java.awt.Color.HSBtoRGB(hsb.h, hsb.s,
 				(float) binsCPArray[Math.min((int) (hsb.b * bins), bins - 1)])).toArray();
-
-		/*
-		 * int[] outputRGBPixelArray =
-		 * Arrays.stream(sourceRGBPixelArray).mapToObj(pixel -> { float[]
-		 * hsbFloatArrayofPixel = new float[] { 0, 0, 0 };
-		 * java.awt.Color.RGBtoHSB(colorModel.getRed(pixel),
-		 * colorModel.getGreen(pixel), colorModel.getBlue(pixel),
-		 * hsbFloatArrayofPixel); return histInstance.new
-		 * HSBPixel(hsbFloatArrayofPixel); }).map(hsb -> { if (hsb.b <= 0.1) {
-		 * hsb.b = (float) binsCPArray[0]; } else if (hsb.b <= 0.2) { hsb.b =
-		 * (float) binsCPArray[1]; } else if (hsb.b <= 0.3) { hsb.b = (float)
-		 * binsCPArray[2]; } else if (hsb.b <= 0.4) { hsb.b = (float)
-		 * binsCPArray[3]; } else if (hsb.b <= 0.5) { hsb.b = (float)
-		 * binsCPArray[4]; } else if (hsb.b <= 0.6) { hsb.b = (float)
-		 * binsCPArray[5]; } else if (hsb.b <= 0.7) { hsb.b = (float)
-		 * binsCPArray[6]; } else if (hsb.b <= 0.8) { hsb.b = (float)
-		 * binsCPArray[7]; } else if (hsb.b <= 0.9) { hsb.b = (float)
-		 * binsCPArray[8]; } else { hsb.b = (float) binsCPArray[9]; } return
-		 * hsb; }).mapToInt(hsb -> java.awt.Color.HSBtoRGB(hsb.h, hsb.s,
-		 * hsb.b)).toArray();
-		 * 
-		 */
 
 		time.now(); // equalize pixels
 		// int[] outputRGBPixelArray = null;
